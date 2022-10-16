af16e6d70b80780b1e027f1e387a5fcad2d82853
Abdullah Juwel <abdullah.juwel@BTSL-Juwel-PSD.btraccl.com>
2022-10-16 11:31:58 +0600

    port changed

--- middlewares/ApiAuth.js
+++ middlewares/ApiAuth.js
@@ -1,13 +1,12 @@
 const jwt = require('jsonwebtoken');
-const { respondWithError, respondWithSuccess } = require('../controllers/admin/ResponseController');
 
 const apiauth = (req, res, next) => {
     try {
         const token = req.headers['authorization'];
-        if (!token) return respondWithError(req, res, msg = 'Invalid Authentication !', data ='', 401);
+        if (!token) return res.status(401).json({ msg: "Invalid Authentication" });
 
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-            if (err) return respondWithError(req, res, msg = 'Invalid Authentication !', data ='', 401);
+            if (err) return res.status(401).json({ msg: "Invalid Authentication" });
 
             req.user = user;
             next();
